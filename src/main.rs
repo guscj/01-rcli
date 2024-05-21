6ea2baa185b00cf2b1c550c6c2289e8961334814
flydicat <381790001@qq.com>
2024-05-22 00:37:21 +0800

    support generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
     match opts.cmd {
         Subcommand::CSV(opts) => {
-            process_csv(&opts.input, &opts.output)?;
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
         }
     }
     Ok(())
